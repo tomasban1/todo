e6dcad9822db1e8004ac5d4cd17f03acb3cc5811
tomasban1 <tomasbanius@gmail.com>
2024-07-15 11:26:33 +0300

    to do + time

--- js/main.js
+++ js/main.js
@@ -4,56 +4,106 @@ const formDom = document.forms[0];
 const textImput = formDom.querySelector('input');
 const submitBtn = formDom.querySelector('button');
 const listDom = document.querySelector('.list');
-const actionDom = listDom.querySelectorAll('button');
-const doneBtn = actionDom[0];
-const editBtn = actionDom[1];
-const deleteBtn = actionDom[2];
 
 
 
+const todoData = [];
 let listNum = 0;
 submitBtn.addEventListener('click', e => {
     e.preventDefault();
     listDom.classList.remove('empty');
-    if (listDom.textContent.includes('Empty')) {
-        listDom.textContent = '';
-    }
 
     h1Dom.textContent = `To do (${listNum += 1})`;
 
-    let HTML = `
-        <article class="item">
-            <div class="text">${textImput.value}</div>
-            <div class="actions">
-                <button>Done</button>
-                <div class="divider"></div>
-                <button>Edit</button>
-                <button>Delete</button>
-            </div>
-        </article>`
-    listDom.innerHTML += HTML;
+    if (textImput.value.length === 0) {
+        return;
+    }
+
+    todoData.push({
+        task: textImput.value,
+        createdAt: Date.now(),
+    });
+
+
     textImput.value = '';
-
-    const actionDom = listDom.querySelectorAll('button');
-    const doneBtn = actionDom[0];
-    const editBtn = actionDom[1];
-    const deleteBtn = actionDom[2];
-
-
-
-
+    renderList();
 });
 
+function renderList() {
+    if (todoData.length === 0) {
+        renderEmptyList();
+    } else {
+        renderTaskList();
+    }
+}
 
-document.body.addEventListener('click', function (e) {
-    if (e.target == submitBtn) {
-        doneBtn.addEventListener('click', () => {
-            console.log('labas');
+function renderEmptyList() {
+    listDom.classList.add('empty');
+    listDom.textContent = 'Empty';
+}
+
+function renderTaskList() {
+    let HTML = ``;
+    for (const todo of todoData) {
+        HTML += `
+            <article class="item">
+                <div class="date">${formatTime(todo.createdAt)}</div>
+                <div class="text">${todo.task}</div>
+                <form class="hidden">
+                <input type="text">
+                    <button type="submit">Update</button>
+                    <button type="button">Cancel</button>
+                </form>
+                <div class="actions">
+                    <button>Done</button>
+                    <div class="divider"></div>
+                    <button>Edit</button>
+                    <button>Delete</button>
+                </div>
+            </article>`
+    }
+    listDom.innerHTML = HTML;
+    listDom.classList.remove('empty');
+
+
+    const articlesDom = document.querySelectorAll('article');
+
+    for (let i = 0; i < articlesDom.length; i++) {
+        const articleDom = articlesDom[i];
+        const editFormDom = articleDom.querySelector('form');
+        const updateImputDom = editFormDom.querySelector('input');
+        const buttonsDom = articleDom.querySelectorAll('button');
+
+        const updateBtn = buttonsDom[0];
+        updateBtn.addEventListener('click', e => {
+            e.preventDefault();
+            todoData[i].task = updateImputDom.value;
+            renderTaskList();
+        });
+
+        const cancelBtn = buttonsDom[1];
+        cancelBtn.addEventListener('click', () => {
+            editFormDom.classList.add('hidden');
+        });
+
+        const editBtn = buttonsDom[3];
+        editBtn.addEventListener('click', () => {
+            editFormDom.classList.remove('hidden')
+        });
+
+        const deleteBtn = buttonsDom[4];
+        deleteBtn.addEventListener('click', () => {
+            todoData.splice(i, 1)
+            h1Dom.textContent = `To do (${listNum -= 1})`;
+            renderTaskList();
         });
     }
-});
+}
 
-/* 
-editBtn.addEventListener('click', () => {
-    console.log('Labas edit');
-}); */
+function formatTime(timeInMs) {
+    const now = new Date();
+    const currentDate = new Date().toJSON().slice(0, 10);
+    const hours = now.getHours();
+    const minutes = now.getMinutes();
+    return `Task created ${currentDate}, ${hours}:${minutes}`
+}
